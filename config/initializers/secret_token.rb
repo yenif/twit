f8f68f6a83a7fb832d5271e9f7e0622b10156418
Brian Finney <bri+git@nfinney.com>
2011-11-11 19:45:49 +0000

    base rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twit::Application.config.secret_token = '89fef0d9459a2cd7e6de5b166cde97186090d27027bc5fef72decdeea15f38eb78c2e17204a1db429e5716900a718a5ed508f833a61f45c1476d234bd0c937ee'
